4a1dad2f60522f6a18b299638d4b2d2b77be65bb
Chris Cantu <pinion31@gmail.com>
2019-09-14 09:43:25 -0500

    Reindent

--- auth_user/views.py
+++ auth_user/views.py
@@ -2,18 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 # Create your views here.
+
+
 def create_user(request):
-  if request.method == 'POST':
-    user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
-    user.save()
-    return redirect('/home')
-  return render(request, 'auth_users/create_user.html')
+    if request.method == 'POST':
+        user = User.objects.create_user(
+            request.POST['username'], request.POST['email'], request.POST['password'])
+        user.save()
+        return redirect('/home')
+    return render(request, 'auth_users/create_user.html')
+
 
 def login_user(request):
-  return render(request, 'auth_users/login.html')
-
-
-
-
-
-
+    return render(request, 'auth_users/login.html')
