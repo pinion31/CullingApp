1654b086a1c758c610c381c096f77d2a2407b6ae
Chris Cantu <pinion31@gmail.com>
2019-09-27 17:40:26 -0500

    Basic QA creation

--- quiz/views.py
+++ quiz/views.py
@@ -24,28 +24,60 @@ class QuestionCreateView(CreateView):
     model = Question 
     template_name = 'quiz/create_question.html'
     questions = []
-    quiz = None
 
     context = {
         'questions': questions,
-        'quiz': quiz,
+        'quiz': None,
         'ques_pk': None,
         'answers': [],
     }
 
     def post(self, request, pk, context=context):
         context['ques_pk'] = pk
+        print(f"quiz is {context['quiz']}")
+        if context['quiz'] == None:
+            context['quiz'] = Quiz.objects.get(pk=pk)
+            print("context['quiz']")
+            print(context['quiz'])
+            # throw error here if quiz is blank
+
         if request.POST.get('question_text'):
+            print('path1')
             context['questions'].append(request.POST.get('question_text'))
             return render(request, 'quiz/create_question.html', context)
         elif request.POST.get('answer_text'):
+            print('path2')
             question_index = int(request.path.split('/')[3])
             context["answers"].append((question_index, request.POST.get('answer_text')))
             return render(request, 'quiz/create_question.html', context)
+        else:
+            print('quiz created')
+            print(context['questions'])
+            #create quiz
+            for index, question in enumerate(context['questions']):
+                answer_set_to_add = []
+                for answer in context['answers']:
+                    if answer[0] == index:
+                        answer_set_to_add.append(answer)
+                Question.objects.create(
+                    quiz_parent=context['quiz'],
+                    question=question,
+                    answers=answer_set_to_add,
+                    correct_answer='temp answer',
+                )
+            return render(request, 'quiz/create_quiz.html')
+            # quiz_parent = models.ForeignKey(Quiz, on_delete=models.CASCADE, null=True)
+            # question = models.TextField(null=False)
+            # correct_answer = models.TextField(null=False)
+            # answers= ArrayField(models.TextField(null=False), default=[])
+            #create question and add answers
+            # add link from question and quiz
+            return render(request, 'quiz/create_quiz.html', context)
 
     def get(self, request, pk, context=context):
         quiz = Quiz.objects.get(pk=pk)
         context['quiz'] = quiz
+        print('path3')
         return render(request, 'quiz/create_question.html', context)
 
 class QuizCreateView(CreateView):
